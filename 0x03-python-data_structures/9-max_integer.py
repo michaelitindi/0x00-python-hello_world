60b460c27e5e28b40963d4c08ab65ac6ebda6074
michaelitindi <michaelitindi@gmail.com>
2023-05-15 13:43:57 -0700

    max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = 0
+    if my_list == None:
+        return None
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
