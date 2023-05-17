85879c940f07a2d750430849eff4465fb65a71c8
michaelitindi <michaelitindi@gmail.com>
2023-05-17 09:08:55 -0700

    used difference

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    combined_set = set_1 | set_2
+    set_one_only = set_1.difference(set_2)
+    set_two_only = set_2.difference(set_1)
+    combined_set = set_one_only | set_two_only
     return combined_set
