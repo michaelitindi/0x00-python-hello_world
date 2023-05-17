781465105f38b3a111ec0cd6489f9dc77cec333b
michaelitindi <michaelitindi@gmail.com>
2023-05-17 11:31:20 -0700

    string format

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    ascending = sorted(a_dictionary.keys())
-    for i in ascending:
-        value = a_dictionary[i]
-        print("{} : {}".format(i, value))
+    for i in sorted(a_dictionary):
+        print("{} : {}".format(i, a_dictionary[i]))
