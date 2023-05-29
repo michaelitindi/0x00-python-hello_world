37bca667728e67a65c8a26b22f55346380bbbf96
michaelitindi <michaelitindi@gmail.com>
2023-05-29 11:34:42 -0700

    print integers only

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        counter = 0
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+    except(ValueError, TypeError):
+        pass
+    print()
+    return counter
