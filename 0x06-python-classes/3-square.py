4819a5bfbbbdcbaab641b92e22f266982fd2d715
michaelitindi <michaelitindi@gmail.com>
2023-05-30 12:17:48 -0700

    function to find area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Class representing a square """
+
+
+class Square:
+    """ Class square with size attribute (private) """
+
+    def __init__(self, size=0):
+        """Initialize square with size
+
+         Args:
+             size: size of square
+
+         """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    def area(self):
+        """ Returns area of a square """
+        return self.__size ** 2
