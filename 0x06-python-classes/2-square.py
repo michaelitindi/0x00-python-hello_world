648babe435d3435a195e53c238d4f11032348134
michaelitindi <michaelitindi@gmail.com>
2023-05-30 12:05:45 -0700

    checks for size and type

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Class representing a square """
+
+
+class Square:
+    """ Class square with size attribute (private) """
+
+    def __init__(self, size):
+        """Initialize square with size
+
+         Args:
+             size: size of square
+
+         """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
