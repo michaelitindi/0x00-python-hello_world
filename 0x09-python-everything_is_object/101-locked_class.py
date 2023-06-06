e61d67a2f4d08f5c5fb1981f2139741a87aef7f7
michaelitindi <michaelitindi@gmail.com>
2023-06-06 07:36:42 -0700

    first name

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creating a class with no object or attribute """
 
+
 class LockedClass:
     """Cannot create instance unless attribute is first name """
-     __slots__ = ["first_name"]
+    __slots__ = ["first_name"]
