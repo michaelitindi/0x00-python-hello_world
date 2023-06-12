9aa4dfbd628eb3e13e0c866c99d033091d48f898
michaelitindi <michaelitindi@gmail.com>
2023-06-12 15:11:15 -0700

    inverted operators

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class has operators inverted"""
+
+
+
+class MyInt(int):
+    """inverted operators """
+
+     def __eq__(self, other):
+         return int.__ne__(self, other)
+
+     def __ne__(self, other):
+         return int.__eq__(self, other)
