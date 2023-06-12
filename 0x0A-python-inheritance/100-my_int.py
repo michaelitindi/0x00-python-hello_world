5e8abfffa8ca170f82a3d515dd9f65790d9a7a3f
michaelitindi <michaelitindi@gmail.com>
2023-06-12 15:16:39 -0700

    fixed style

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,12 +2,10 @@
 """ class has operators inverted"""
 
 
-
 class MyInt(int):
     """inverted operators """
+    def __eq__(self, other):
+        return int.__ne__(self, other)
 
-     def __eq__(self, other):
-         return int.__ne__(self, other)
-
-     def __ne__(self, other):
-         return int.__eq__(self, other)
+    def __ne__(self, other):
+        return int.__eq__(self, other)
