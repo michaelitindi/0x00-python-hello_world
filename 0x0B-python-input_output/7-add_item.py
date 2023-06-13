cf4ab0d89c2dc86146b29af059642f6e75bd4764
michaelitindi <michaelitindi@gmail.com>
2023-06-13 12:33:30 -0700

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that adds all arguments to a Python list"""
+
+
+import sys
+import json
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    data = load_from_json_file(filename)
+except FileNotFoundError:
+    data = []
+
+data.extend(sys.argv[1:])
+save_to_json_file(data, filename)
