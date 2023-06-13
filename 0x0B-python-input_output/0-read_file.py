0021035c9b69d6fb9c6ebc93e4a5462c03166ced
michaelitindi <michaelitindi@gmail.com>
2023-06-13 08:34:51 -0700

    read and print out

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that reads a text file """
+
+
+def read_file(filename=""):
+    """prints it to stdout """
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content)
