5a0abc0e5aeae6b81503754deb188cdea0c5615f
michaelitindi <michaelitindi@gmail.com>
2023-06-13 13:15:18 -0700

    student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Class student with dict representation"""
+
+
+class Student:
+    """attributes and methods """
+
+     def __init__(self, first_name, last_name, age):
+         """ intialized student"""
+         self.first_name = first_name
+         self.last_name = last_name
+         self.age = age
+
+     def to_json(self):
+        """returns dict representation"""
+        return self.__dict__
