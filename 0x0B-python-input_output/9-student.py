76b6f8447ea8880da6753c14e355fab001c0dc87
michaelitindi <michaelitindi@gmail.com>
2023-06-13 13:20:46 -0700

    student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,12 +5,12 @@
 class Student:
     """attributes and methods """
 
-     def __init__(self, first_name, last_name, age):
-         """ intialized student"""
-         self.first_name = first_name
-         self.last_name = last_name
-         self.age = age
+    def __init__(self, first_name, last_name, age):
+        """ intialized student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-     def to_json(self):
+    def to_json(self):
         """returns dict representation"""
         return self.__dict__
