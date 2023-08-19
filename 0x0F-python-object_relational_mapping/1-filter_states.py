94d9e6de7ed6cab7182fafbf39e54b4bf3b93076
michaelitindi <michaelitindi@gmail.com>
2023-08-19 13:40:59 +0000

    fixed some pycodestlye issues

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that lists all states from the database hbtn_0e_0_usa starting with N"""
+"""all states from the database hbtn_0e_0_usa starting with N"""
 
 import MySQLdb
 import sys
@@ -14,7 +14,8 @@ if __name__ == "__main__":
         db=sys.argv[3]
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 
+                BINARY 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
