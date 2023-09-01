43e9356d219658e8b15de6b1080e824674880f7e
michaelitindi <michaelitindi@gmail.com>
2023-09-01 14:52:53 -0700

    package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" fetches url"""
+import requests
+
+if __name__ == "__main__":
+    resp = requests.get('https://alx-intranet.hbtn.io/status')
+    body = resp.text
+
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
