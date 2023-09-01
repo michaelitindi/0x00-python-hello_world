47db4839e252c7f73a238ae1b3f00c9493ace68d
michaelitindi <michaelitindi@gmail.com>
2023-09-01 14:57:30 -0700

    body response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 if __name__ == "__main__":
     resp = requests.get('https://alx-intranet.hbtn.io/status')
     body = resp.text
-
+    print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
