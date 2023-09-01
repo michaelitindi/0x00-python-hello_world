6abf84e7f9255ce4050a395c62a7ecacda9798da
michaelitindi <michaelitindi@gmail.com>
2023-09-01 11:57:46 -0700

    fetch and read

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("- type: {}".format(type(body)))
+    print("- content: {}".format(body))
+    print("- utf8 content: {}".format(body.decode("utf-8")))
